f91e29d2b931fbbc2bdecfe684e577354e14cfd7
Nbelankar <nandkishor.belankar@laminaar.in>
2023-12-16 18:10:58 +0530

    create vite project

---
+++ 01vite-react/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <div className="card">
+        <h1>Hello React with Vite</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
