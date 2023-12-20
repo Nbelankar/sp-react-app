d5245df64b7e96e26f8c69f0e652392a91c399f7
Nbelankar <nandkishor.belankar@laminaar.in>
2023-12-20 11:52:01 +0530

    added card

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -2,10 +2,24 @@ import "./App.css";
 import OurTeam from "./component/ourTeam/ourTeam";
 
 function App() {
+  const people = [
+    {
+      name: "Leslie Alexander",
+      role: "Founder",
+      imageUrl:
+        "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+    {
+      name: "Leslie Jullie",
+      role: "Co-Founder / CEO",
+      imageUrl:
+        "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    },
+  ];
   return (
     <>
-      <h1 className='bg-green-400'>Tailwind Test </h1>
-      <OurTeam />
+      <h1 className="bg-green-400">Tailwind Test </h1>
+      <OurTeam people={people} />
     </>
   );
 }
